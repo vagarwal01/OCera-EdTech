0f9d6de83a3d543d7b06f1b8349fc0b2bd0c0cbd
Vedangi Agarwal <48343993+vagarwal01@users.noreply.github.com>
2021-02-26 21:50:08 +0530

    Update google-passport-setup.js

--- google-passport-setup.js
+++ google-passport-setup.js
@@ -21,9 +21,9 @@ passport.deserializeUser(function(user, done) {
 });
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    clientID: "254924267599-a56c8so2jhoddjfhmj9dqqsevsn5f8de.apps.googleusercontent.com",
+    clientSecret: "jHE1qpMhkSM8LOWKZ5u0fWx6",
+    callbackURL: "/google/callback",
     passReqToCallback: true
 }, function(request, accessToken, refreshToken, profile, done) {
     return done(null, profile)
